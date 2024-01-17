7b194013e16108c04aeb54256efb55939528737d
pdvjuan <juanp@phoenixdeventures.com>
2024-01-17 09:48:19 -0800

    commit new deps
    15 in and 43 out still

---
+++ aws.config.js
@@ -0,0 +1,11 @@
+export default () => {
+  return {
+    Auth: {
+      region: "us-west-2",
+      userPoolId: process.env.COGNITO_USER_POOL || "us-east-1_ITgABQ3nm",
+      userPoolWebClientId:
+        process.env.COGNITO_WEB_CLIENT || "5oqqtrhr5t6n1m9ik5ejlmfma4",
+      authenticationFlowType: "USER_PASSWORD_AUTH",
+    },
+  };
+};
